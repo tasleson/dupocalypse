tasleson/blk-archive

Recovery and offset tracking improvements
This commit combines two major enhancements to archive reliability:

1. Recovery Checkpoint System for Crash-Safety:
   - Implements periodic checkpoints during pack operations (--sync-interval)
   - Atomic checkpoint files with Blake2b-64 checksums for corruption detection
   - Automatic archive restoration to last checkpoint state on startup
   - Tracks: hashes file, index files, data slabs, and offset files
   - Atomic stream directory creation (.tmp_<id> -> final name on success)
   - Universal cleanup of incomplete operations on archive access
   - Ensures consistency after crashes, signals (SIGTERM/SIGKILL), or power failures

2. Memory-Mapped slab offsets:

   The slab-offsets file originally stored a contiguous sequence of little-
   endian u64 values. That format made it hard to detect truncation or
   corruption, so we added a footer to validate file completeness and
   integrity.

   This is the only file that does not use the SlabFile abstraction; we keep
   it as a single contiguous array of offsets for simplicity and fast indexed
   access. To reduce memory usage (the previous design held all offsets in
   RAM), we switched to memory-mapping (mmap) the offsets sequence.

   We have not yet measured the impact of this change on pack/unpack/verify
   performance.

   - Complete rewrite of src/slab/offsets.rs with mmap-based architecture
   - Lazy offset loading with memory mapping for scalability (~2 PiB archives)
     if we're located on a FAT32 FS
   - CRC64-ECMA-182 checksums with rolling digest for incremental writes
   - 24-byte footer format: MAGIC | COUNT | CRC64
   - Hybrid in-memory/on-disk design: mmap for existing, Vec for new offsets
   - Supports append-only writes without full file rewrites
   - Comprehensive validation with corruption detection

Implementation Details:
   - New recovery.rs module for checkpoint management
   - Extended archive.rs with checkpoint/restore logic
   - Updated slab file

src/main.rs
@@ -5,11 +5,13 @@ use std::process::exit;
 use std::sync::Arc;
 use thinp::report::*;
 
+use blk_archive::archive::flight_check;
 use blk_archive::create;
 use blk_archive::dump_stream;
 use blk_archive::list;
 use blk_archive::output::Output;
 use blk_archive::pack;
+use blk_archive::recovery;
 use blk_archive::unpack;
 
 //-----------------------
@@ -134,7 +136,19 @@ fn main_() -> Result<()> {
                         .value_name("DELTA_DEVICE")
                         .num_args(1),
                 )
-                .arg(data_cache_size.clone()),
+                .arg(data_cache_size.clone())
+                .arg(
+                    Arg::new("SYNC_POINT_SECS")
+                        .help("Number of seconds before creating a sync point in the archive. Smaller \
+                              values allow you to restart a pack with less data needing to be added to \
+                              archive at the cost of slower pack times")
+                        .required(false)
+                        .long("sync-point-secs")
+                        .value_name("SYNC_POINT_SECS")
+                        .num_args(1)
+                        .default_value("15")
+                        .value_parser(clap::value_parser!(u64).range(1..)),
+                ),
         )
         .subcommand(
             Command::new("unpack")
@@ -189,6 +203,26 @@ fn main_() -> Result<()> {
         report: report.clone(),
         json: matches.get_flag("JSON"),
     });
+
+    // Check for and apply recovery checkpoint before processing any command
+    // This handles interrupted operations by truncating files to last known-good state
+    if let Some((subcommand_name, sub_matches)) = matches.subcommand() {
+        if let Some(archive_path) = sub_matches.get_one::<String>("ARCHIVE") {
+            let checkpoint_path =
+                std::path::Path::new(archive_path).join(recovery::check_point_file());
+            if recovery::RecoveryCheckpoint::exists(&checkpoint_path) {
+                let checkpoint = recovery::RecoveryCheckpoint::read(&checkpoint_path)?;
+                checkpoint.apply(archive_path)?;
+            }
+
+            // Do a preflight check before proceeding to ensure the archive is in a hopefully
+            // good state (skip for create command as archive doesn't exist yet)
+            if subcommand_name != "create" {
+                flight_check(archive_path)?;
+            }
+        }
+    }
+
     match matches.subcommand() {
         Some(("create", sub_matches)) => {
             create::run(sub_matches, report)?;

tests/recovery.rs
@@ -1,0 +1,194 @@
+use anyhow::Result;
+use std::path::PathBuf;
+use std::process::{Command as StdCommand, Stdio};
+use std::time::Duration;
+
+mod common;
+use common::blk_archive::*;
+use common::fixture::*;
+use common::random::Pattern;
+use common::test_dir::*;
+use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
+use std::process::Child;
+
+fn blk_archive_pack(archive: &BlkArchive, input: PathBuf) -> Result<Child> {
+    const BLK_ARCHIVE_BIN: &str = env!("CARGO_BIN_EXE_blk-archive");
+    Ok(StdCommand::new(BLK_ARCHIVE_BIN)
+        .arg("pack")
+        .arg("-a")
+        .arg(archive.archive_path())
+        .arg(&input)
+        .arg("--sync-point-secs")
+        .arg("1")
+        .arg("-j")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?)
+}
+
+fn kill_process(pid: i32) {
+    signal::kill(Pid::from_raw(pid), Signal::SIGKILL).unwrap();
+}
+
+#[test]
+fn test_interrupt_after_checkpoint() -> Result<()> {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    let mut td = TestDir::new()?;
+    let file_size = 1024 * 1024 * 1024u64;
+
+    let archive = create_archive(&mut td, false)?;
+    let input = create_input_file(&mut td, file_size, 1, Pattern::Lcg)?;
+
+    let checkpoint_path = archive.archive_path().join("recovery.checkpoint");
+    let checkpoint_start = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    let child = blk_archive_pack(&archive, input.clone())?;
+
+    // Spin waiting for the checkpoint file to get updated then hit it with a signal, then
+    // let it complete
+
+    loop {
+        let current = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+        if current != checkpoint_start {
+            eprintln!("check point modified");
+            break;
+        }
+    }
+
+    // The pack operation is creating a checkpoint ~ every 1 sec
+    let delay = rng.gen_range(100..=500);
+    eprintln!("delaying {delay} ms");
+    std::thread::sleep(Duration::from_millis(delay));
+
+    kill_process(child.id() as i32);
+
+    let output = child.wait_with_output()?;
+
+    let checkpoint_after_signal = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    // Maybe we should dump stdout & stderr?
+
+    assert!(!output.status.success());
+
+    // Now, lets make sure we can handle whatever corruption may have been done to archive
+    let child = blk_archive_pack(&archive, input)?;
+    let output = child.wait_with_output()?;
+
+    if !output.status.success() {
+        eprintln!("pack did not completes successfully! {}", output.status);
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+
+        assert!(output.status.success());
+    } else {
+        let checkpoint_after_complete = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+        assert!(checkpoint_after_signal < checkpoint_after_complete);
+
+        eprintln!("SUCCESS - debug data");
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_interrupt_before_checkpoint() -> Result<()> {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    let mut td = TestDir::new()?;
+    let file_size = 1024 * 1024 * 1024u64;
+
+    let archive = create_archive(&mut td, false)?;
+    let input = create_input_file(&mut td, file_size, 1, Pattern::Lcg)?;
+
+    let checkpoint_path = archive.archive_path().join("recovery.checkpoint");
+    let checkpoint_start = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    let child = blk_archive_pack(&archive, input.clone())?;
+
+    // The pack operation is creating a checkpoint ~ every 1 sec
+    let delay = rng.gen_range(200..800);
+    eprintln!("delaying {delay} ms");
+    std::thread::sleep(Duration::from_millis(delay));
+
+    kill_process(child.id() as i32);
+
+    let output = child.wait_with_output()?;
+
+    let checkpoint_after_signal = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    // Maybe we should dump stdout & stderr?
+
+    assert!(!output.status.success());
+    assert_eq!(checkpoint_start, checkpoint_after_signal);
+
+    // Now, lets make sure we can handle whatever corruption may have been done to archive
+    let child = blk_archive_pack(&archive, input)?;
+    let output = child.wait_with_output()?;
+
+    if !output.status.success() {
+        eprintln!("pack did not completes successfully! {}", output.status);
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+
+        assert!(output.status.success());
+    } else {
+        let checkpoint_after_complete = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+        assert!(checkpoint_after_signal < checkpoint_after_complete);
+
+        eprintln!("SUCCESS - debug data");
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    Ok(())
+}
+
+/// Test that checkpoint files are created and updated
+#[test]
+fn test_checkpoint_file_creation() -> Result<()> {
+    let mut td = TestDir::new()?;
+    let file_size = 64 * 1024 * 1024u64; // 64 MiB
+
+    // Create archive
+    let archive = create_archive(&mut td, false)?;
+
+    // Check that initial checkpoint was created
+    let checkpoint_path = archive.archive_path().join("recovery.checkpoint");
+    assert!(
+        checkpoint_path.exists(),
+        "Initial checkpoint should be created"
+    );
+
+    let initial_metadata = std::fs::metadata(&checkpoint_path)?;
+    let initial_modified = initial_metadata.modified()?;
+
+    eprintln!("Initial checkpoint created at: {:?}", initial_modified);
+
+    // Wait a moment to ensure timestamps differ
+    std::thread::sleep(Duration::from_millis(100));
+
+    // Pack a file (should update checkpoint at end)
+    let input = create_input_file(&mut td, file_size, 1, Pattern::Lcg)?;
+    let response = archive.pack(&input)?;
+
+    eprintln!("Packed file, stream ID: {}", response.stream_id);
+
+    // Check that checkpoint was updated
+    let final_metadata = std::fs::metadata(&checkpoint_path)?;
+    let final_modified = final_metadata.modified()?;
+
+    eprintln!("Final checkpoint modified at: {:?}", final_modified);
+
+    assert!(
+        final_modified > initial_modified,
+        "Checkpoint should be updated after pack"
+    );
+
+    Ok(())
+}

tasleson/dupocalypse

   RAM), we switched to memory-mapping (mmap) the offsets sequence if
   the size > 256MiB.
   performance when the slab index gets large.

src/main.rs
@@ -3,11 +3,13 @@ use std::process::exit;
 use std::sync::Arc;
 use thinp::report::*;
 
+use dupocalypse::archive::flight_check;
 use dupocalypse::create;
 use dupocalypse::dump_stream;
 use dupocalypse::list;
 use dupocalypse::output::Output;
 use dupocalypse::pack;
+use dupocalypse::recovery;
 use dupocalypse::unpack;
 
 //-----------------------
@@ -41,6 +43,25 @@ where
 fn main_() -> Result<()> {
     let cli = cli::build_cli();
     let matches = cli.get_matches();
+
+    // Check for and apply recovery checkpoint before processing any command
+    // This handles interrupted operations by truncating files to last known-good state
+    if let Some((subcommand_name, sub_matches)) = matches.subcommand() {
+        if let Some(archive_path) = sub_matches.get_one::<String>("ARCHIVE") {
+            let checkpoint_path =
+                std::path::Path::new(archive_path).join(recovery::check_point_file());
+            if recovery::RecoveryCheckpoint::exists(&checkpoint_path) {
+                let checkpoint = recovery::RecoveryCheckpoint::read(&checkpoint_path)?;
+                checkpoint.apply(archive_path)?;
+            }
+
+            // Do a preflight check before proceeding to ensure the archive is in a hopefully
+            // good state (skip for create command as archive doesn't exist yet)
+            if subcommand_name != "create" {
+                flight_check(archive_path)?;
+            }
+        }
+    }
 
     match matches.subcommand() {
         Some(("create", sub_matches)) => {

tests/recovery.rs
@@ -1,0 +1,194 @@
+use anyhow::Result;
+use std::path::PathBuf;
+use std::process::{Command as StdCommand, Stdio};
+use std::time::Duration;
+
+mod common;
+use common::dupocalypse::*;
+use common::fixture::*;
+use common::random::Pattern;
+use common::test_dir::*;
+use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
+use std::process::Child;
+
+fn dupocalpyse(archive: &Dupocalypse, input: PathBuf) -> Result<Child> {
+    const DUPOCALYPSE_BIN: &str = env!("CARGO_BIN_EXE_dupocalypse");
+    Ok(StdCommand::new(DUPOCALYPSE_BIN)
+        .arg("pack")
+        .arg("-a")
+        .arg(archive.archive_path())
+        .arg(&input)
+        .arg("--sync-point-secs")
+        .arg("1")
+        .arg("-j")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?)
+}
+
+fn kill_process(pid: i32) {
+    signal::kill(Pid::from_raw(pid), Signal::SIGKILL).unwrap();
+}
+
+#[test]
+fn test_interrupt_after_checkpoint() -> Result<()> {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    let mut td = TestDir::new()?;
+    let file_size = 1024 * 1024 * 1024u64;
+
+    let archive = create_archive(&mut td, false)?;
+    let input = create_input_file(&mut td, file_size, 1, Pattern::Lcg)?;
+
+    let checkpoint_path = archive.archive_path().join("recovery.checkpoint");
+    let checkpoint_start = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    let child = dupocalpyse(&archive, input.clone())?;
+
+    // Spin waiting for the checkpoint file to get updated then hit it with a signal, then
+    // let it complete
+
+    loop {
+        let current = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+        if current != checkpoint_start {
+            eprintln!("check point modified");
+            break;
+        }
+    }
+
+    // The pack operation is creating a checkpoint ~ every 1 sec
+    let delay = rng.gen_range(100..=500);
+    eprintln!("delaying {delay} ms");
+    std::thread::sleep(Duration::from_millis(delay));
+
+    kill_process(child.id() as i32);
+
+    let output = child.wait_with_output()?;
+
+    let checkpoint_after_signal = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    // Maybe we should dump stdout & stderr?
+
+    assert!(!output.status.success());
+
+    // Now, lets make sure we can handle whatever corruption may have been done to archive
+    let child = dupocalpyse(&archive, input)?;
+    let output = child.wait_with_output()?;
+
+    if !output.status.success() {
+        eprintln!("pack did not completes successfully! {}", output.status);
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+
+        assert!(output.status.success());
+    } else {
+        let checkpoint_after_complete = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+        assert!(checkpoint_after_signal < checkpoint_after_complete);
+
+        eprintln!("SUCCESS - debug data");
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_interrupt_before_checkpoint() -> Result<()> {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    let mut td = TestDir::new()?;
+    let file_size = 1024 * 1024 * 1024u64;
+
+    let archive = create_archive(&mut td, false)?;
+    let input = create_input_file(&mut td, file_size, 1, Pattern::Lcg)?;
+
+    let checkpoint_path = archive.archive_path().join("recovery.checkpoint");
+    let checkpoint_start = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    let child = dupocalpyse(&archive, input.clone())?;
+
+    // The pack operation is creating a checkpoint ~ every 1 sec
+    let delay = rng.gen_range(200..800);
+    eprintln!("delaying {delay} ms");
+    std::thread::sleep(Duration::from_millis(delay));
+
+    kill_process(child.id() as i32);
+
+    let output = child.wait_with_output()?;
+
+    let checkpoint_after_signal = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+
+    // Maybe we should dump stdout & stderr?
+
+    assert!(!output.status.success());
+    assert_eq!(checkpoint_start, checkpoint_after_signal);
+
+    // Now, lets make sure we can handle whatever corruption may have been done to archive
+    let child = dupocalpyse(&archive, input)?;
+    let output = child.wait_with_output()?;
+
+    if !output.status.success() {
+        eprintln!("pack did not completes successfully! {}", output.status);
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+
+        assert!(output.status.success());
+    } else {
+        let checkpoint_after_complete = std::fs::metadata(&checkpoint_path)?.modified().unwrap();
+        assert!(checkpoint_after_signal < checkpoint_after_complete);
+
+        eprintln!("SUCCESS - debug data");
+        eprintln!("stdout = {}", String::from_utf8_lossy(&output.stdout));
+        eprintln!("stderr = {}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    Ok(())
+}
+
+/// Test that checkpoint files are created and updated
+#[test]
+fn test_checkpoint_file_creation() -> Result<()> {
+    let mut td = TestDir::new()?;
+    let file_size = 64 * 1024 * 1024u64; // 64 MiB
+
+    // Create archive
+    let archive = create_archive(&mut td, false)?;
+
+    // Check that initial checkpoint was created
+    let checkpoint_path = archive.archive_path().join("recovery.checkpoint");
+    assert!(
+        checkpoint_path.exists(),
+        "Initial checkpoint should be created"
+    );
+
+    let initial_metadata = std::fs::metadata(&checkpoint_path)?;
+    let initial_modified = initial_metadata.modified()?;
+
+    eprintln!("Initial checkpoint created at: {:?}", initial_modified);
+
+    // Wait a moment to ensure timestamps differ
+    std::thread::sleep(Duration::from_millis(100));
+
+    // Pack a file (should update checkpoint at end)
+    let input = create_input_file(&mut td, file_size, 1, Pattern::Lcg)?;
+    let response = archive.pack(&input)?;
+
+    eprintln!("Packed file, stream ID: {}", response.stream_id);
+
+    // Check that checkpoint was updated
+    let final_metadata = std::fs::metadata(&checkpoint_path)?;
+    let final_modified = final_metadata.modified()?;
+
+    eprintln!("Final checkpoint modified at: {:?}", final_modified);
+
+    assert!(
+        final_modified > initial_modified,
+        "Checkpoint should be updated after pack"
+    );
+
+    Ok(())
+}